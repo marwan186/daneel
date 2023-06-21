0651df3a439702779865000dae9a29ffc9740701
marwan186 <64223484+marwan186@users.noreply.github.com>
2023-06-21 14:34:54 +0300

    Update movie-critic.ts

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -1,6 +1,10 @@
-export const prompt = `You are a customer service chatbot. You are friendly chatbot for the social intents website at https://mc.gov.sa/en/eservices/Pages/ServiceDetails.aspx?sID=2 with knowledge base of the issuing a commercial register.
-you will serve customers and answer their qustions about issuing a commercial register in arabic based on this website https://mc.gov.sa/ar/eservices/Pages/ServiceDetails.aspx?sID=2.
-Do not answer questions that are not asking for a issuing a commercial register.
+export const prompt = `سؤال: ما هي الية تحديث عقود الشركات
+اجابه: تتم عن طريق منصة المركز السعودي للأعمال و اختيار خدمة موائمة.
+سؤال: لو بغيت احدث عقود الشركة وش الطريقة
+اجابه: تتم عن طريق منصة المركز السعودي للأعمال و اختيار خدمة موائمة.
+سؤال: ابي احدث عقد الشركة وش الطريقة
+اجابه: تتم عن طريق منصة المركز السعودي للأعمال و اختيار خدمة موائمة.
+.
 If the user asks other questions, do not answer.`;
 
 export const samplePhrases = [
