d556529c55335682a2655d764387b1a290c51fe3
marwan186 <64223484+marwan186@users.noreply.github.com>
2023-06-22 13:43:11 +0300

    Update movie-critic.ts

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -4,14 +4,130 @@ Question: What is the mechanism for updating corporate contracts?
 It is done through the Saudi Business Center platform and choosing a suitable service.
 Question: What is the mechanism for documenting the Memorandum of Association or the decisions of the partners?
 In the event that the business requirements are available, the documentation will be done through the Absher platform, and in the event that it is not available, the documentation will be done with the authorized representative of the Ministry of Commerce or with the notary
+Question: Is there a specific percentage for each partner in a (professional company)?
+Yes, the percentage of the licensed Saudi partner must not be less than (25%) of the professional company’s capital, and the percentage of the licensed partners must not be less than (70%)
+Question: When trying to issue a sub-registry for a Saudi institution, a message (domestication) appears. What is the procedure?
+The note on localization and correcting the status of the facility must be fulfilled.
+Question: Is it possible to practice more than one commercial activity on one commercial registry?
+Yes, it is possible to practice more than one commercial activity in one commercial registry.
+Question: Is it possible to appoint a foreign director on the commercial registry of the corporation?
+It is not possible to appoint a foreign director on the commercial register of the corporation.
+Question: What are the steps for issuing a commercial registration?
+1- Login to the Saudi Business Center platform
+2- Log in through the website account or the national access
+3- Enterprise data
+4- The applicant shall specify the owner of the establishment or the delegate
+5- Determine the type of trade name
+6- Choose the type of establishment and activity in the trade name
+8- Fill in the address and contact information
+9- An acknowledgment of the validity of the data entered
+10- Click on submit application
+Question: What are the conditions for issuing a commercial register?
+1- He must be at least 18 years old.
+2- The owner should not be a government employee.
+3- The capital should not be less than 5000 riyals.
+Question: What are the documents required to issue a commercial register?
+nothing
+Question: How long does the service of issuing a commercial register take to be implemented?
+immediately
+Question: Where can I renew the commercial registration?
+website
+Question: How much is the fee for issuing a commercial register?
+200 Saudi riyals for the main and 100 Saudi riyals for the secondary
+Question: What are the steps to renew the commercial registration?
+Click on Start Service.
+Enter the required information.
+Login to the account (username and password).
+Click on the My Commercial Records icon.
+Click on the options in front of the commercial register.
+Click on the “Renewal of Commercial Registration” option.
+Determining the duration of the commercial registration from one to five years.
+A confirmation summary window appears on the data entered on the service before sending the request.
+A payment invoice will be issued and sent as a text message to your phone.
+The bill is paid through various payment channels. For more information about SADAD service.
+Question: What are the conditions for renewing the commercial registration?
+The owner should not be a government employee.
+To be at least 18 years old.
+Question: What are the documents required to renew the commercial registration?
+nothing
+Question: How long does it take to renew a commercial registration?
+immediately
+Question: Where can I renew the commercial registration?
+website
+Question: How much is the commercial registration renewal service fee?
+200 riyals for the principal (per year) + the Chamber of Commerce fees are added according to the type of activity for the commercial register and the grade for each year. 100 riyals for minors (per year) + Chamber of Commerce fees are added according to the type of activity for the commercial register and the degree for each year.
+Question: What are the payment methods?
+Payment methods are:
+1- Payment
+2- Mada
+3- Visa
+4- Master Card
 سؤال: ما هي الية تحديث عقود الشركات
 تتم عن طريق منصة المركز السعودي للأعمال و اختيار خدمة موائمة.
 سؤال: ماهي الية توثيق عقد التأسيس او قرارات الشركاء
 في حال توفر متطلبات الأعمال سيتم التوثيق عبر منصة أبشر وفي حال عدم توفرها فيتم التوثيق لدى معتمد وزارة التجارة أو لدى كاتب العدل
+سؤال: هل يوجد نسبة محددة لكل شريك في (شركة مهنية)
+نعم، يجب ألا تقل نسبة الشريك السعودي المرخص عن (25%) من رأس مال الشركة المهنية، ألا تقل نسبة الشركاء المرخصين عن (70%)
+سؤال:  عند محاولة إصدار سجل فرعي لمؤسسة سعودية تظهر رسالة (توطين) ما الإجراء 
+يجب استيفاء الملاحظة الخاصة بالتوطين وتصحيح وضع المنشأة. 
+سؤال: هل يمكن ممارسة أكثر من نشاط تجاري على سجل تجاري واحد 
+نعم بالإمكان ممارسة أكثر من نشاط تجاري على سجل تجاري واحد. 
+سؤال: هل يمكن تعيين مدير أجنبي على السجل التجاري للمؤسسة 
+لا يمكن تعيين مدير أجنبي على السجل التجاري للمؤسسة. 
+سؤال: وش خطوات اصدار السجل التجاري 
+1- الدخول على منصة المركز السعودي للأعمال 
+2- تسجيل الدخول عن طريق حساب الموقع الإلكتروني او النفاذ الوطني 
+3- بيانات المؤسسة 
+4- تحديد مقدم الطلب مالك مؤسسة او مفوض 
+5- تحديد نوع الاسم التجاري 
+6- اختيار نوع المنشأة و النشاط في الاسم التجاري 
+8- تعبئة العنوان وبيانات الاتصال 
+9- اقرار بصحة البينات المدخلة 
+10- الضغط على تقديم الطلب 
+سؤال: ماهي شروط اصدار سجل تجاري 
+1- أن لا يقل العمر عن 18 سنة . 
+2- أن لا يكون المالك موظف حكومي . 
+3- أن لا يقل رأس المال عن 5000 ريال. 
+سؤال: ماهي المستندات المطلوبه لاصدار سجل تجاري 
+لايوجد 
+سؤال: كم مدة تنفيذ خدمة اصدار سجل تجاري 
+فوري 
+سؤال: من وين اقدر اجدد السجل التجاري  
+الموقع الالكتروني
+سؤال: كم رسوم اصدار السجل التجاري 
+200ريال سعودي للرئيسي و 100ريال سعودي للفرعي 
+سؤال: ماهي خطوات تجديد السجل التجاري 
+الضغط على ابدأ الخدمة. 
+إدخال المعلومات المطلوبة. 
+الدخول إلى الحساب (اسم المستخدم وكلمة المرور) . 
+الضغط على أيقونة سجلاتي التجارية . 
+الضغط على الاختيارات الموجودة أمام السجل التجاري . 
+الضغط على خيار ""تجديد السجل التجاري"". 
+تحديد مدة السجل التجاري من سنة حتى خمس سنوات. 
+تظهر نافذة ملخص التأكيد على البيانات المدخلة على الخدمة قبل إرسال الطلب. 
+يتم اصدار فاتورة سداد وإرسالها كرساله نصيه لهاتفك. 
+يتم سداد الفاتورة عبر قنوات السداد المختلفة. ولمزيد من المعلومات عن خدمة سداد \
+سؤال: ماهي شروط تجديد السجل التجاري 
+أن لا يكون المالك موظف حكومي . 
+أن لا يقل العمر عن 18 سنة. 
+سؤال: ماهي المستندات المطلوب لتجديد السجل التجاري 
+لايوجد 
+سؤال: كم الفترة المستغرقة لتجديد سجل تجاري  
+فوري 
+سؤال: من وين اقدر اجدد السجل التجاري  
+الموقع الالكتروني  
+سؤال:كم رسوم خدمة تجديد السجل التجاري  
+٢٠٠ ريال للرئيسي(للسنة الواحدة) + تضاف عليها رسوم الغرفة التجارية حسب نوع النشاط للسجل التجاري والدرجة لكل سنة. ١٠٠ ريال للفرعي (للسنة الواحدة) + تضاف عليها رسوم الغرفة التجارية حسب نوع النشاط للسجل التجاري والدرجة لكل سنة. 
+سؤال: ماهي طرق الدفع  
+طرق الدفع هي: 
+1- سداد 
+2- مدى 
+3- فيزا 
+4- ماستر كارد 
 `;
 export const samplePhrases = [
   "كم نسبة الشريك في الشركة المهنيه",
   "ماهي الية تحديث عقود الشركات؟",
-  "شلون.>احدث العقود؟",
+  "ماهي طرق الدفع",
   
 ];
